74a24bcfdae7593d243250f8c2196fc7385d3ba9
Isus <xatab228@gmail.com>
2023-11-14 22:13:07 +0300

    # add fetching fn

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-
 # import telebot
 # from telebot import types
 # import time
-import ccxt
+# import ccxt
 
-#bot = telebot.TeleBot('6944341639:AAHVIrqTCyhCJYdbnR3TU0WEiTOhtSXpbW8')
-
+# bot = telebot.TeleBot('6944341639:AAHVIrqTCyhCJYdbnR3TU0WEiTOhtSXpbW8')
 
 
 import requests
@@ -13,15 +11,14 @@ import pandas as pd
 import time
 
 # List of available exchange platforms
-platforms = {
+PLATFORMS_API = {
     'Bybit': 'https://api.bybit.com/spot/quote/v1/ticker/price',
     'Binance': 'https://api.binance.com/api/v3/ticker/price',
     'Bitget': 'https://api.bitget.com/api/v2/spot/market/tickers'
-
 }
 
+SYMBOLS_API = 'https://api.huobi.pro/v1/common/currencys'
 
-symbols = ['BTCUSDT']
 
 def custom_comparison(dictionares, keyValue):
     results = {}
@@ -36,113 +33,72 @@ def custom_comparison(dictionares, keyValue):
                 results[place][place_two] = 100 - (float(values[keyValue]) / float(values_two[keyValue])) * 100
     return results
 
-# def custom_comparison(dictionares, keyValue):
-#     results = {}
-#     platforms = list(dictionares.keys())
-#     for item in range(len(platforms)):
-#         place = platforms[item]
-#         values = dictionares[place]
-#         results[place] = {}
-#         for j in range(item+1, len(platforms)):
-#             place_two = platforms[j]
-#             values_two = dictionares[place_two]
-#             results[place][place_two] = 100 - (float(values[keyValue]) / float(values_two[keyValue])) * 100
-#     return results
+
+def normalize(custom_dictionary, key_price='price', key_symbol='symbol'):
+    return {
+        'price': custom_dictionary[key_price],
+        'symbol': custom_dictionary[key_symbol]
+    }
 
 
-def get_ticker_data(exchange, symbol):
+def get_data(url):
     try:
-        if exchange == 'Binance':
-            url = platforms[exchange] + '?symbol=' + symbol
-            response = requests.get(url)
-            data = response.json()
-            return data
-        elif exchange == 'Bybit':
-            url = platforms[exchange] + '?symbol=' + symbol
-            response = requests.get(url)
-            data = response.json()
-            return data['result']
-        elif exchange == 'Bitget':
-            url = platforms[exchange] + '?symbol=' + symbol
-            response = requests.get(url)
-            data = response.json()
-            return data['data']
+        response = requests.get(url)
+        return response.json()
+    except Exception as e:
+        print(f"Error fetching data from {e}")
+
+
+def get_symbols_data():
+    try:
+        data = get_data(SYMBOLS_API)
+        local_symbols = []
+        for local_symbol in data['data'][1:]:
+            local_symbols.append(local_symbol.upper() + "USDT")
+        return local_symbols
+    except Exception as e:
+        print(f"Error fetching symbols data from {e}")
+
+
+def get_platform_data(exchange, local_symbol):
+    try:
+        url = PLATFORMS_API[exchange] + '?symbol=' + local_symbol
+        return get_data(url)
+    except Exception as e:
+        print(f"Error fetching platform data from {e}")
+
+
+def get_ticker_data(exchange, local_symbol):
+    try:
+        data = get_platform_data(exchange, local_symbol)
+        # print(exchange,local_symbol, data)
+        if exchange == 'Binance' and ('symbol' in data):
+            return normalize(data)
+        elif exchange == 'Bybit' and data['ret_code'] == 0:
+            return normalize(data['result'])
+        elif exchange == 'Bitget' and data['msg'] == 'success':
+            return normalize(data['data'][0], 'lastPr')
+        return -1
+
     except Exception as e:
         print(f"Error fetching data from {exchange}: {e}")
 
 
-
 while True:
     try:
+        symbols = get_symbols_data()
+        # print(symbols)
         for symbol in symbols:
             price_comparison = {}
-            for platform in platforms:
+            for platform in PLATFORMS_API:
                 ticker_data = get_ticker_data(platform, symbol)
-                price_comparison[platform] = ticker_data
+                if ticker_data != -1:
+                    price_comparison[platform] = ticker_data
             result_comparison = custom_comparison(price_comparison, 'price')
-            #print(f"Price Comparison fn: {result_comparison}")
+            # print(f"Price Comparison fn: {result_comparison}")
             print(f"{symbol} Price Comparison: {result_comparison}")
-            #print(f"{symbol} Price Comparison: {price_comparison}")
-        time.sleep(10) # Wait for 10 seconds before fetching data again
+            # print(f"{symbol} Price Comparison: {price_comparison}")
+        time.sleep(10)
     except KeyboardInterrupt:
         print("\nExiting...")
         break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_binance_quotes():
-#     url = "https://api.binance.com/api/v3/ticker/price"
-#     response = requests.get(url)
-#     data = response.json()
-#     return data
-# data = get_binance_quotes()
-# print(data)
-#
-#
-# print('-----------------------------------------------------------------------------')
-#
-# def get_bybit_price():
-#     url1 = "https://api.bybit.com/spot/quote/v1/ticker/price"
-#     response1 = requests.get(url1)
-#     data1 = response1.json()
-#     return data1['result'],['price']
-# data1 = get_bybit_price()
-# print(data1)
-
-# @bot.message_handler(func=lambda message: True)
-# def send_bitcoin_price(message):
-#     response = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
-#     data = response.json()
-#     price = data['price']
-#     response1 = requests.get('https://api.bybit.com/spot/quote/v1/ticker/price?symbol=BTCUSDT')
-#     data1 = response1.json()
-#     price1 = data1['result']['price']
-#     bot.reply_to(message, f"The current price of Bitcoin on Binance is {price}/{price1}USDT.")
-# bot.polling()
-
